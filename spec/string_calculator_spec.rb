138f4a4491b22755720fdefe032d6d5e880ed257
Ravi Mehra <ravi.lib17@gmail.com>
2025-08-01 14:41:18 +0530

    Adding and passing single number string test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns a single number if only one is provided' do
+      expect(calculator.add('7')).to eq(7)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
   def add(string)
     return 0 if string.empty?
+
+    numbers = string.split(',')
+    numbers.first.to_i if numbers.size == 1
   end
 end
